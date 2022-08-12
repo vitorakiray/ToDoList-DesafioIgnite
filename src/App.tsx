f157d2e8a7a26d79477a468baf5a46a059d569ba
Vitor Akira <vitorakira1@gmail.com>
2022-08-12 15:45:55 -0300

    checkbox change isDone value

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,31 @@
-
 import { Header } from './components/Header';
+import { Task } from './components/Task';
+import { ChangeEvent, FormEvent, useState } from 'react';
 
+import { v4 as uuidv4 } from 'uuid';
 import clipboardImg from './assets/Clipboard.svg';
 import { PlusCircle } from 'phosphor-react';
 
 import styles from './App.module.css';
 import './global.css';
-import { Task } from './components/Task';
-import { ChangeEvent, FormEvent, useState } from 'react';
 
 export function App() {
-  const [tasks, setTasks] = useState([''])
+  const [tasks, setTasks] = useState([{
+    id: uuidv4(),
+    title: 'Ola',
+    isDone: false,
+  }])
 
   const [newTaskText, setNewTaskText] = useState('');
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault();
 
-    setTasks([...tasks, newTaskText]);
+    setTasks([...tasks, {
+      id: uuidv4(),
+      title: newTaskText,
+      isDone: false,
+    }]);
 
     setNewTaskText('');
   }
@@ -28,11 +36,40 @@ export function App() {
     setNewTaskText(event.target.value);
   }
 
+  function deleteTask(taskIdToDelete: string) {
+    const taskWithoutDeletedOne = tasks.filter(task => {
+      return task.id !== taskIdToDelete;
+    })
+
+    setTasks(taskWithoutDeletedOne);
+  }
+
+  function checkTask(taskIdToCheck: string) {
+    const tasksWithUpdatedTaskChecked = tasks.map(task => {
+      if (task.id === taskIdToCheck) {
+        return {
+          ...task,
+          isDone: !task.isDone
+        }
+
+      } else {
+        return task
+      }
+    })
+
+    setTasks(tasksWithUpdatedTaskChecked)
+    console.log(tasksWithUpdatedTaskChecked)
+  }
+
+  const quantityOfTasks = tasks.length;
+  const isQuantityOfTasksZero = quantityOfTasks === 0;
+
   return (
     <>
       <Header />
 
       <div className={styles.wrapper}>
+
         <form className={styles.taskForm} onSubmit={handleCreateNewTask}>
           <textarea
             placeholder="Adicione uma nova tarefa"
@@ -43,23 +80,33 @@ export function App() {
         </form>
 
         <div className={styles.numberOfTasks}>
-          <p>Tarefas criadas <span>{tasks.length}</span></p>
+          <p>Tarefas criadas <span>{quantityOfTasks}</span></p>
           <p>Concluídas <span>0</span></p>
         </div>
 
-        {tasks.map(task => (
-          <Task
-            task={task}
-          />
-        ))}
+        {isQuantityOfTasksZero ?
+          <div className={styles.emptyContentBox}>
+            <img src={clipboardImg} alt="Imagem de uma prancheta" />
+            <p>
+              <strong>Você ainda não tem tarefas cadastradas</strong>
+              Crie tarefas e organize seus itens a fazer
+            </p>
+          </div>
+          :
+          tasks.map(task => (
+            <Task
+              key={task.id}
+              taskId={task.id}
+              isDone={task.isDone}
+              title={task.title}
+              onDeleteTask={deleteTask}
+              onCheckTask={checkTask}
+            />
+          ))
+        }
 
-        <div className={styles.emptyContentBox}>
-          <img src={clipboardImg} alt="Imagem de uma prancheta" />
-          <p>
-            <strong>Você ainda não tem tarefas cadastradas</strong>
-            Crie tarefas e organize seus itens a fazer
-          </p>
-        </div>
+
+
       </div>
 
     </>
--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -3,18 +3,37 @@ import { Trash } from 'phosphor-react'
 import styles from './Task.module.css';
 
 interface TaskProps {
-    task: string;
+    title: string;
+    isDone: boolean;
+    taskId: string;
+    onDeleteTask: (id: string) => void;
+    onCheckTask: (id: string) => void;
 }
 
-export function Task({ task }: TaskProps) {
+export function Task({ isDone, title, taskId, onDeleteTask, onCheckTask }: TaskProps) {
+    function handleDeleteTask() {
+        onDeleteTask(taskId);
+    }
+
+    function handleCheckTask() {
+        onCheckTask(taskId)
+    }
+
+
     return (
         <div className={styles.taskContainer}>
             <div className={styles.taskContent}>
-                <input type="checkbox" />
-                <p>{task}</p>
+                <input
+                    type="checkbox"
+                    onChange={handleCheckTask}
+                />
+                <p>{title}</p>
             </div>
 
-            <Trash size={24} />
+            <button className={styles.deleteButton} onClick={handleDeleteTask}>
+                <Trash size={24} />
+            </button>
+
         </div>
     )
 }
